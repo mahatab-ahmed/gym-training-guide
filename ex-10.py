3c0a30a12f22760cdb6b86effdda883c414b3018
Mahatab Ahmed <72667512+mahatab-ahmed@users.noreply.github.com>
2020-12-14 19:24:38 +0530

    Update ex-10.py

--- ex-10.py
+++ ex-10.py
@@ -1,19 +1,4 @@
 
-# Create a library class
-# display book
-# lend book - (who owns the book if not present)
-# add book
-# return book
-
-# HarryLibrary = Library(listofbooks, library_name)
-
-
-#dictionary (books-nameofperson)
-
-# create a main function and run an infinite while loop asking
-# users for their input
-
-
 class Library:
     def __init__(self, list, name):
         self.booksList = list
